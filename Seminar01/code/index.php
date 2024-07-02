517edaf528489eefa6547de58c898dee7d5e62a4
Valerii_Panov <val250640@mail.ru>
2024-07-03 09:58:19 +1000

    sem01

---
+++ Seminar01/code/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$name = "Admin";
+
+var_dump($name);
+
+var_dump([1,2,3,4]);
+
+$count = '38 попугаев';
+var_dump($count);
+
+$x = 1;
+$y = 2;
+echo "x = $x, y = $y<br/>";
+
+[$y,$x] = [$x, $y];
+
+echo "x = $x, y = $y";
